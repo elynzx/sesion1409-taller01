82174aab43bed6254dce029f744cefb56a3c3baf
Elynzx <rpascual06@outlook.com>
2026-09-14 20:24:16 -0500

    Ej01 declaración de variables

---
+++ ejercicio01.js
@@ -0,0 +1,11 @@
+let nombre = "Evelyn";
+let edad = 34;
+let esEstudiante = true;
+let ciudad = "Lima";
+let hobby = "Planificar";
+
+console.log(`Mi nombre es ${nombre}`);
+console.log(`Tengo ${edad} años`);
+console.log(`¿Soy estudiante?: ${esEstudiante}`);
+console.log(`Vivo en ${ciudad}`);
+console.log(`Mi hobby es ${hobby}`);
