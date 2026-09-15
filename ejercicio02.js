0a7caee5eeb2c73e871a2334fb2fd6bb44a9e903
Elynzx <rpascual06@outlook.com>
2026-09-14 20:24:35 -0500

    Ej02 operadores aritmeticos

---
+++ ejercicio02.js
@@ -0,0 +1,16 @@
+let num1 = 20;
+let num2 = 6;
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+let residuo = num1 % num2;
+let potencia = num1 ** num2;
+
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicación: " + multiplicacion);
+console.log("División: " + division);
+console.log("Residuo: " + residuo);
+console.log("Potencia: " + potencia);
